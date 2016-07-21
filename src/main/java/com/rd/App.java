c25d0788f30b6c359f3376112df5471cf0ad96cd
Rajith Delantha <rajithgama@gmail.com>
2016-07-21 12:42:52 +0530

    Initial commit

---
+++ src/main/java/com/rd/App.java
@@ -0,0 +1,46 @@
+package com.rd;
+
+import java.util.Random;
+
+public class App {
+    public String method1(int num) {
+        if (num > 7)
+            return "More than 7";
+
+        return "Less than 7";
+    }
+
+    public int doInt() {
+        return new Random().nextInt(8);
+    }
+
+    public void anotherVeryUsefulBusinessMethod(String whatToDo){
+        if ("work hard".equals(whatToDo)) {
+            System.out.println("Working hard now");
+        } else {
+            System.out.println("I will not " + whatToDo);
+        }
+    }
+
+    public Long methodImportant() {
+        return Math.round(1000*Math.random());
+    }
+
+    public int methodNotSoImportant(int param) {
+        if (param > 10) {
+            return param;
+        }
+
+        return 0;
+    }
+
+    public String methodOfUnknownImportance() {
+        return "Ululam Athenas Ferre";
+    }
+
+    public static void main(String args[]) {
+        System.out.println("****************************************");
+        System.out.println("hello world");
+        System.out.println("****************************************");
+    }
+}
---
+++ src/test/java/com/rd/AppTest.java
@@ -0,0 +1,45 @@
+package com.rd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Alexey Z
+ */
+public class AppTest {
+    private final App app = new App();
+
+    @Test
+    public void testMethod1() throws Exception {
+        Assert.assertEquals(app.method1(5), "Less than 7");
+    }
+
+    @Test
+    public void testDoIt() throws Exception {
+
+    }
+
+    @Test
+    public void testAnotherVeryUsefulBusinessMethod() throws Exception {
+        app.anotherVeryUsefulBusinessMethod("Bake potatos");
+        app.anotherVeryUsefulBusinessMethod("Run integration tests");
+    }
+
+    @Test
+    public void testMethodImportant() throws Exception {
+        Assert.assertTrue(1001 > app.methodImportant());
+    }
+
+    @Test
+    public void testMethodNotSoImportant() throws Exception {
+        Assert.assertEquals(11, app.methodNotSoImportant(11));
+        Assert.assertEquals(0, app.methodNotSoImportant(9));
+    }
+
+    @Test
+    public void testMethodOfUnknownImportance() throws Exception {
+        // todo Implement test
+    }
+
+
+}
