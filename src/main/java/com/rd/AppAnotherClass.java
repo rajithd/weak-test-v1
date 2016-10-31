92a589ca6dc000d0600e9c5a03cb8214dc6e99ed
Rajith Delantha <rajithgama@gmail.com>
2016-10-31 12:15:49 +0530

    added 31-1

---
+++ src/main/java/com/rd/AppAnotherClass.java
@@ -0,0 +1,13 @@
+package com.rd;
+
+public class AppAnotherClass {
+
+    public void execute() {
+        System.out.println("Exe");
+        System.out.println("Exe");
+        System.out.println("Exe");
+        System.out.println("Exe");
+        System.out.println("Exe");
+    }
+
+}
