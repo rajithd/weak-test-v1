27d1f9083b431812e68519425d50df32d2a45e94
rajithd-aurea <rajith.delantha@aurea.com>
2016-11-01 13:48:26 +0530

    Create App2.java

---
+++ src/main/java/com/rd/App2.java
@@ -0,0 +1,13 @@
+package com.rd;
+
+public class App2 {
+
+    public void execute() {
+        System.out.println("Exe1");
+        System.out.println("Exe1");
+        System.out.println("Exe1");
+        System.out.println("Exe1");
+        System.out.println("Exe1");
+    }
+
+}
