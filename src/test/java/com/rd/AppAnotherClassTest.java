256fe06e020651e00614a2be5c0bd1f2cf053604
Rajith Delantha <rajithgama@gmail.com>
2016-11-03 11:15:17 +0530

    add test case for AppAnotherClass

---
+++ src/test/java/com/rd/AppAnotherClassTest.java
@@ -0,0 +1,25 @@
+package com.rd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rajith on 11/3/16.
+ */
+public class AppAnotherClassTest {
+
+    private AppAnotherClass appAnotherClass;
+
+    @Before
+    public void setUp() throws Exception {
+        appAnotherClass = new AppAnotherClass();
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        appAnotherClass.execute();
+        assertTrue(true);
+    }
+}
